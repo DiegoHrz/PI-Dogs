cca934d0cf07a8c2f1a700fe9ae305e1cb00b3fc
DiegoHrz <hernandezwdiego@gmail.com>
2023-12-07 01:04:55 -0500

    Add handlers module

---
+++ api/src/handlers/dogsHandler.js
@@ -0,0 +1,35 @@
+const dogsHandler = {};
+
+dogsHandler.getDogsHandler = (req, res) => {
+  try {
+    res.status(200).send("get 1 dog");
+  } catch (error) {
+    res.status(404).json({ error: error.message });
+  }
+};
+
+dogsHandler.getDogsRaza = (req, res) => {
+  try {
+    res.status(200).send("get 2 dogos");
+  } catch (error) {
+    res.status(404).json({ error: error.message });
+  }
+};
+
+dogsHandler.getDogsName = (req, res) => {
+  try {
+    res.status(200).send("get 3");
+  } catch (error) {
+    res.status(404).json({ error: error.message });
+  }
+};
+
+dogsHandler.postDogs = (req, res) => {
+  try {
+    res.status(200).send("post");
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+};
+
+module.exports = dogsHandler;
--- api/src/routes/dogsRoute.js
+++ api/src/routes/dogsRoute.js
@@ -1,36 +1,16 @@
 const { Router } = require("express");
 const dogsRoute = Router();
 
-dogsRoute.get("/", (req, res) => {
-  try {
-    res.status(200).send("get 1");
-  } catch (error) {
-    res.status(404).json({ error: error.message });
-  }
-});
+const dogsHandler = require('../handlers/dogsHandler')
+const {getDogsHandler, getDogsRaza, getDogsName, postDogs} = dogsHandler
 
-dogsRoute.get("/:idRaza", (req, res) => {
-  try {
-    res.status(200).send("get 2");
-  } catch (error) {
-    res.status(404).json({ error: error.message });
-  }
-});
 
-dogsRoute.get("/name", (req, res) => {
-  try {
-    res.status(200).send("get 3");
-  } catch (error) {
-    res.status(404).json({ error: error.message });
-  }
-});
+dogsRoute.get("/", getDogsHandler);
 
-dogsRoute.post("/", (req, res) => {
-  try {
-    res.status(200).send("post");
-  } catch (error) {
-    res.status(400).json({ error: error.message });
-  }
-});
+dogsRoute.get("/:id", getDogsRaza);
+
+dogsRoute.get("/name", getDogsName );
+
+dogsRoute.post("/", postDogs);
 
 module.exports = dogsRoute;
