fdec933fadfcefd6b7cdb21e0732f7c1ad6fc039
DiegoHrz <hernandezwdiego@gmail.com>
2023-12-12 19:34:22 -0500

    Add temperaments handler

--- api/src/handlers/temperamentsHandler.js
+++ api/src/handlers/temperamentsHandler.js
@@ -3,6 +3,7 @@ const temperamentsHandler = {};
 
 temperamentsHandler.getTemperamentsHandler = async (req, res) => {
   try {
+    const response = await getTemperaments();
     res.status(200).json(response);
   } catch (error) {
     res.status(404).json({ error: error.message });
