337b898bbe810dcd199c4323c51058f8306b9290
DiegoHrz <hernandezwdiego@gmail.com>
2023-12-15 19:29:22 -0500

    Add switch and routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,26 @@
 import "./App.css";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
+import Landing from "./Views/Landing/Landing";
+import Details from "./Views/Details/Details";
+import Home from "./Views/Home/Home";
+import Create from "./Views/Create/Create";
+import Navbar from "./Components/Navbar/Navbar";
 //in version 6 : import {BrowserRouter, Route, Routes}
-
 
 function App() {
   return (
     <div className="App">
+      {/*siempre es obligatorio que todas las rutas esten dentro del BrowserRouter mientras que si sale del switch no ha problema*/}
       <BrowserRouter>
         <Switch>
-          <Route path={"/"} component={} ></Route>
-          <Route></Route>
-          <Route></Route>
-          <Route></Route>
-          <Route></Route>
-          <Route></Route>
+          {/*o es la 1ra ruta / y renderiza Landing o renderiza el componente navbar en todas las rutas */}
+          <Route exact path={"/"} component={Landing}></Route>
+          <Route path={""} component={Navbar}></Route>
+        </Switch>
+        <Switch>
+          <Route path={"/home"} component={Home}></Route>
+          <Route path={"/create"} component={Create}></Route>
+          <Route path={"/details"} component={Details}></Route>
         </Switch>
       </BrowserRouter>
     </div>
